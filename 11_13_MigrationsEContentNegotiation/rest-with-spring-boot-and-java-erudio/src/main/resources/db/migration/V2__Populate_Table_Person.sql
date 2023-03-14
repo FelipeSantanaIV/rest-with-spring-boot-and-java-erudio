5aa9df3fc0f26ded890b7301f273896db121e25b
Felipe Sant'Ana <felipe.santana1995@hotmail.com>
2023-03-13 21:07:01 -0300

    Outro commit

---
+++ 11_13_MigrationsEContentNegotiation/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `adress`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo', 'Felipe', 'Male', 'Santana'),
+	(2, 'R.dsadsadsadsa', 'Thuyanne', 'Feminino', 'Santana'),
+	(4, 'INdia', 'Indira', 'Male', 'Gandhui'),
+	(7, 'Brazil', 'Gabriel', 'Male', 'Zuado'),
+	(8, 'Brazil', 'FelipeS', 'Male', 'Santananna');
